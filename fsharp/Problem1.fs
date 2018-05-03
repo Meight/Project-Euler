bb0f326b422f3c146b976a8ea29db91e5244b28b
matt.leboucher <matt.leboucher@gmail.com>
2018-05-02 23:26:58 -0400

    Adds F# solution to problem 1.

---
+++ fsharp/Problem1.fs
@@ -0,0 +1,9 @@
+(*
+** This file contains a solution to the problem number 2 of the Project Euler, last modified on 5/2/2018.
+** Copyright (c) Matthieu Le Boucher. All rights reserved.
+*)
+
+open System
+
+let result = Seq.sum (seq { for n in 1..999 do if n % 3 = 0 || n % 5 = 0 then yield n })
+printfn "%A" result
