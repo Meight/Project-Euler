85d57a06e28269d80669d231136e67e358faa71e
matt.leboucher <matt.leboucher@gmail.com>
2018-05-03 11:22:15 -0400

    Adds Python solution to problem 3.

---
+++ python/Problem3.py
@@ -0,0 +1,18 @@
+#
+# This file contains a solution to the problem number 3 of the Project Euler, last modified on 5/3/2018.
+# Copyright (c) Matthieu Le Boucher. All rights reserved.
+#
+
+def result():
+    n = 600_851_475_143
+    currentFactor = 2
+
+    while currentFactor * currentFactor < n:
+        while n % currentFactor == 0:
+            n = n / currentFactor
+
+        currentFactor = currentFactor + 1
+
+    return n
+
+print(result())
