5395e27a7e58170c373faf87097919a679e2d070
Meight <matt.leboucher@gmail.com>
2018-05-05 17:57:24 -0400

    Adds F# solution to problem 6.

---
+++ fsharp/Problem6.fs
@@ -0,0 +1,11 @@
+(*
+** This file contains a solution to the problem number 6 of the Project Euler, last modified on 5/5/2018.
+** Copyright (c) Matthieu Le Boucher. All rights reserved.
+*)
+
+open System
+
+let n = 100
+let result = (3 * (pown n 4) + 2 * (pown n 3) - 3 * (pown n 2) - 2 * n) / 12
+
+printfn "%A" result
