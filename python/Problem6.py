1896bf644f43f9288b707b465e0b733c58573eca
Meight <matt.leboucher@gmail.com>
2018-05-05 17:48:16 -0400

    Adds Python solution to problem 6.

---
+++ python/Problem6.py
@@ -0,0 +1,14 @@
+#
+# This file contains a solution to the problem number 6 of the Project Euler, last modified on 5/5/2018.
+# Copyright (c) Matthieu Le Boucher. All rights reserved.
+#
+
+def result():
+    n = 100
+
+    result = 3 * pow(n, 4) + 2 * pow(n, 3) - 3 * pow(n, 2) - 2 * n
+    result = result / 12
+
+    return result
+
+print(result())
