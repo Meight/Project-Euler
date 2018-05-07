bc337aa6910e6792af089d727c09bb140532608d
Meight <matt.leboucher@gmail.com>
2018-05-07 00:39:35 -0400

    Appends results only.

--- java/runTests.sh
+++ java/runTests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Running tests on Java solutions..."
-java -Dfile.encoding=UTF-8 -classpath "C:\Program Files\Java\jdk1.8.0_74\jre\lib\*;C:\production\Project-Euler" Test.RunTests > tests.log
+java -Dfile.encoding=UTF-8 -classpath "C:\Program Files\Java\jdk1.8.0_74\jre\lib\*;C:\production\Project-Euler" Test.RunTests &> "$1"
 
 read -p "Press any key to continue... " -n1 -s
