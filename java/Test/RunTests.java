3be008bca3eccf874865471c7f6133b52c050567
Meight <matt.leboucher@gmail.com>
2018-05-06 23:23:55 -0400

    Uses a private solution log.

--- java/Test/RunTests.java
+++ java/Test/RunTests.java
@@ -9,20 +9,33 @@ package Test;
 import Problem.EulerSolution;
 
 import java.lang.annotation.Annotation;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class RunTests {
-
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
         final List<Class<?>> solutions = ReflectionHelper.findClassesImplementing(EulerSolution.class,
                 EulerSolution.class.getPackage());
 
+        List<SolutionLog> solutionLogs = new ArrayList<>();
+
         for (Class solutionClass : solutions)
-            readSolution(solutionClass);
+            solutionLogs.add(generateSolutionLog(solutionClass));
+
+        formatLogs(solutionLogs);
     }
 
-    public static void readSolution(Class solutionClass) throws IllegalAccessException, InstantiationException {
+    private static void formatLogs(List<SolutionLog> solutionLogs) {
+        for (SolutionLog solutionLog : solutionLogs) {
+            System.out.println("[Problem " + solutionLog.getProblemNumber() + "] " + solutionLog.getExecutionTimeMilliseconds() + " ms");
+        }
+    }
+
+    private static SolutionLog generateSolutionLog(Class solutionClass)
+            throws IllegalAccessException, InstantiationException {
         if (EulerSolution.class.isAssignableFrom(solutionClass)) {
             EulerSolution eulerSolution = (EulerSolution) solutionClass.newInstance();
             if (solutionClass.isAnnotationPresent(Solution.class)) {
@@ -30,14 +43,44 @@ public class RunTests {
                 Solution solution = (Solution) annotation;
 
                 long startTime = System.nanoTime();
-                if (solution.answer() == eulerSolution.solution()) {
-                    System.out.println("[" + solutionClass.getName() + "] Correct answer, " +
-                            "obtained in " + (System.nanoTime() - startTime) / 1_000 + " ms.");
-                } else
-                    System.out.println("Incorrect answer.");
+                return new SolutionLog(eulerSolution.getClass().getName(),
+                        solution.answer() == eulerSolution.solution(),
+                        System.nanoTime() - startTime);
             }
-        } else {
-            System.out.println("Class given is not an Euler solution.");
+        }
+
+        System.out.println("Class given is not an Euler solution.");
+        return null;
+    }
+
+    private static class SolutionLog {
+        private int problemNumber;
+        private long executionTime;
+        private boolean isCorrect;
+
+        public SolutionLog(String problemName, boolean isCorrect, long executionTime) {
+            Matcher matcher = Pattern.compile("\\d+").matcher(problemName);
+            matcher.find();
+
+            this.problemNumber = Integer.valueOf(matcher.group());
+            this.executionTime = executionTime;
+            this.isCorrect = isCorrect;
+        }
+
+        public int getProblemNumber() {
+            return problemNumber;
+        }
+
+        public long getExecutionTime() {
+            return executionTime;
+        }
+
+        public long getExecutionTimeMilliseconds() {
+            return executionTime / 1_000;
+        }
+
+        public boolean isCorrect() {
+            return isCorrect;
         }
     }
 }
