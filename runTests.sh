16093a64ba24ed3f129b16ac985d6e5e6a3f94b6
Meight <matt.leboucher@gmail.com>
2018-05-07 00:47:51 -0400

    Formats text.

--- runTests.sh
+++ runTests.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-RED='\033[0;31m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
 NC='\033[0m' # No Color
-printf "I ${RED}love${NC} Stack Overflow\n"
 
 genericScriptName="runTests.sh"
 folders="java"
 testsOutputDestination="tests.txt"
 
-echo "Running all tests..."
+printf "Running all tests...\n\n"
 
 for folder in $folders; do
-    echo "Running tests from folder '$folder'"...
+    printf "Running tests from folder '${RED}$folder${NC}'...\n"
+
     $folder/$genericScriptName "$testsOutputDestination"
+
+    printf "> ${GREEN} Done. Results saved to $testsOutputDestination.${NC}\n\n"
 done
 
 read -p "Press any key to continue... " -n1 -s
