accbb2efa8c89ab1ec833f41141e8f62a4877869
Matt Elbé <matt.leboucher@gmail.com>
2018-05-08 17:11:01 -0400

    Expected format for parsing.

--- java/Test/LogsFormatter.java
+++ java/Test/LogsFormatter.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 public class LogsFormatter {
     public static void formatLogs(List<RunTests.SolutionLog> solutionLogs) {
         for (RunTests.SolutionLog solutionLog : solutionLogs) {
-            System.out.println("[Problem " + solutionLog.getProblemNumber() + "] " + String.format("%d µs",
+            System.out.println(solutionLog.getProblemNumber() + " " + String.format("%d",
                     TimeUnit.NANOSECONDS.toMicros(solutionLog.getExecutionTime())));
         }
     }
