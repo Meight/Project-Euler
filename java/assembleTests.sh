f1011d595b57468a956e32d9a3468ed4f6f47d60
Meight <matt.leboucher@gmail.com>
2018-05-09 18:52:31 -0400

    Removes polluting echo.

--- java/assembleTests.sh
+++ java/assembleTests.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-echo "Assembling all test files..."
-java -Dfile.encoding=UTF-8 -classpath "C:\Program Files\Java\jdk1.8.0_74\jre\lib\*;C:\production\Project-Euler" Test.LogsFormatter &> "$1"
-
+java -Dfile.encoding=UTF-8 -classpath "C:\Program Files\Java\jdk1.8.0_74\jre\lib\*;C:\production\Project-Euler" Test.LogsFormatter "$@"
