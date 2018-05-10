d13aef470f684837eea4e8fa376ede949de46cd6
Meight <matt.leboucher@gmail.com>
2018-05-09 21:47:48 -0400

    Creates an utils library.

---
+++ python/EulerLib.py
@@ -0,0 +1,26 @@
+import math
+
+def isPrime(number):
+    """
+    Checks that a given number is a prime number.
+    """
+    if number == 1:
+        return False
+
+    if number < 4:
+        return True
+
+    if number % 2 == 0:
+        return False
+
+    if number < 9:
+        return True
+
+    if number % 3 == 0:
+        return False
+
+    for i in range(5, int(math.sqrt(number)) + 1, 6):
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+
+    return True
