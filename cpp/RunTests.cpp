4a4a7a426c3a22e3936e0c170c64387b0d38d3ad
Matt Elbé <matt.leboucher@gmail.com>
2018-05-20 16:07:36 -0400

    Updates tests.

--- cpp/RunTests.cpp
+++ cpp/RunTests.cpp
@@ -5,13 +5,19 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
-#include <stdio.h>
 #include <fstream>
 
 #include "EulerSolution.h"
 #include "Problem001.cpp"
 #include "Problem002.cpp"
 #include "Problem003.cpp"
+#include "Problem004.cpp"
+#include "Problem005.cpp"
+#include "Problem006.cpp"
+#include "Problem007.cpp"
+#include "Problem008.cpp"
+#include "Problem009.cpp"
+#include "Problem014.cpp"
 
 using namespace std;
 
@@ -26,16 +32,29 @@ int main(int argc, char *argv[]) {
     EulerSolution* solutions[] = {
             new Problem001(),
             new Problem002(),
-            new Problem003()
+            new Problem003(),
+            new Problem004(),
+            new Problem005(),
+            new Problem006(),
+            new Problem007(),
+            new Problem008(),
+            new Problem009(),
+            new Problem014(),
     };
+
 
     for (EulerSolution *solution : solutions) {
         auto startTime = std::chrono::high_resolution_clock::now();
-        bool isCorrect = solution->solution() == solution->answer();
+        double computedSolution = solution->solution();
         std::chrono::duration<double> duration = std::chrono::high_resolution_clock::now() - startTime;
         time_resolution elapsed = std::chrono::duration_cast<time_resolution>(duration);
 
-        testLogs << solution->number() << " " << elapsed.count() / 1000.0 << (isCorrect ? "" : "*** FAILURE. ***") << endl;
+        bool isCorrect = computedSolution == solution->answer();
+
+        if(isCorrect)
+            testLogs << solution->number() << " " << elapsed.count() / 1000.0 << endl;
+        else
+            testLogs << solution->number() << " *** FAILURE: " << computedSolution << " vs. " << solution->answer() << endl;
     }
 
     testLogs.close();
