59e3278d1f87f6f1d8a0639134e56bd6dea44096
Matt Elbé <matt.leboucher@gmail.com>
2018-05-19 22:14:11 -0400

    Adds C++ solution to problem 1.

---
+++ cpp/EulerSolution.h
@@ -0,0 +1,14 @@
+/*
+ * @author Matthieu Le Boucher <matt.leboucher@gmail.com>
+ */
+
+#ifndef CPP_EULERSOLUTION_H
+#define CPP_EULERSOLUTION_H
+
+class EulerSolution {
+public:
+    virtual int number() = 0;
+    virtual double solution() = 0;
+};
+
+#endif
---
+++ cpp/Problem001.cpp
@@ -0,0 +1,22 @@
+#include "EulerSolution.h"
+
+/*
+ * @author Matthieu Le Boucher <matt.leboucher@gmail.com>
+ */
+
+class Problem001 : public EulerSolution {
+public:
+    int number() override {
+        return 1;
+    }
+
+    double solution() override {
+        int sum = 0;
+
+        for (int n = 3; n < 1000; n++)
+            if (n % 3 == 0 || n % 5 == 0)
+                sum += n;
+
+        return sum;
+    }
+};
