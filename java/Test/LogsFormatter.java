c4b24b897eca01400af716ddd0f6668e257dffad
Matt Elbé <matt.leboucher@gmail.com>
2018-06-11 22:14:27 -0400

    Refactoring of logs formatter.

--- java/Test/LogsFormatter.java
+++ java/Test/LogsFormatter.java
@@ -17,58 +17,55 @@ public class LogsFormatter {
     }
 
     public static void main(String[] args) {
-        Map<Integer, List<SolutionLog>> problemSolutions = new HashMap<>();
+        Map<Integer, Map<String, SolutionLog>> problemSolutions = new HashMap<>();
         List<String> languages = new ArrayList<>();
 
         for (String filePath : args) {
-            filePath = filePath.trim();
-            // Create new entry in solutions hash map.
-            String currentLanguage = filePath.split("\\.")[0].split("_")[1];
-            languages.add(currentLanguage);
-
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
-                int problemsCount = 1;
-                String currentLine;
-
-                while ((currentLine = bufferedReader.readLine()) != null) {
-                    String[] splitLine = currentLine.split(" ");
-
-                    int problemNumber = Integer.parseInt(splitLine[0]);
-
-                    if (!problemSolutions.containsKey(problemNumber))
-                        problemSolutions.put(problemNumber, new ArrayList<>());
-
-                    if (problemNumber != problemsCount) {
-                        for (int i = problemsCount; i < problemNumber; i++) {
-                            if (!problemSolutions.containsKey(i))
-                                problemSolutions.put(i, new ArrayList<>());
-
-                            problemSolutions.get(i).add(new SolutionLog(currentLanguage, i, false, -1));
-                        }
-
-                        problemsCount = problemNumber;
-                    }
-
-                    SolutionLog solutionLog =
-                            new SolutionLog(
-                                    currentLanguage,
-                                    problemNumber,
-                                    true, // All solutions here are always correct.
-                                    Double.parseDouble(splitLine[1]));
-
-                    problemSolutions.get(problemNumber).add(solutionLog);
-
-                    problemsCount++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            parseFile(filePath, languages, problemSolutions);
         }
+
+        Collections.sort(languages);
 
         System.out.println(parseFileHeader(languages));
 
         for (int problemNumber : problemSolutions.keySet()) {
-            System.out.println(parseProblemLine(problemNumber, problemSolutions.get(problemNumber)));
+            System.out.println(parseProblemLine(problemNumber, languages, problemSolutions.get(problemNumber)));
+        }
+    }
+
+    private static void parseFile(String filePath,
+                                  List<String> languages,
+                                  Map<Integer, Map<String, SolutionLog>> problemSolutions) {
+        filePath = filePath.trim();
+        // Create new entry in solutions hash map.
+        String currentLanguage = filePath.split("_")[1].split("\\.")[0];
+        languages.add(currentLanguage);
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+            int problemsCount = 1;
+            String currentLine;
+
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                String[] splitLine = currentLine.split(" ");
+
+                int problemNumber = Integer.parseInt(splitLine[0]);
+
+                if (!problemSolutions.containsKey(problemNumber))
+                    problemSolutions.put(problemNumber, new HashMap<>());
+
+                SolutionLog solutionLog =
+                        new SolutionLog(
+                                currentLanguage,
+                                problemNumber,
+                                true, // All solutions here are always correct.
+                                Double.parseDouble(splitLine[1]));
+
+                problemSolutions.get(problemNumber).put(currentLanguage, solutionLog);
+
+                problemsCount++;
+            }
+        } catch (IOException e) {
+            System.out.println("Filename: " + filePath);
         }
     }
 
@@ -81,18 +78,14 @@ public class LogsFormatter {
         return header.toString();
     }
 
-    private static String parseProblemLine(int problemNumber, List<SolutionLog> problemSolutions) {
+    private static String parseProblemLine(int problemNumber,
+                                           List<String> languages,
+                                           Map<String, SolutionLog> problemSolutions) {
         StringBuilder line = new StringBuilder(problemNumber + " ");
 
-        problemSolutions.sort((o1, o2) -> {
-            if (o1.getLanguage().equals(o2.getLanguage()))
-                return 0;
-            return o1.getLanguage().compareTo(o2.getLanguage());
-        });
-
-        for (SolutionLog solutionLog : problemSolutions) {
-            if (solutionLog.getExecutionTime() != -1)
-                line.append(solutionLog.getExecutionTime()).append(" ");
+        for (String language : languages) {
+            if (problemSolutions.containsKey(language))
+                line.append(problemSolutions.get(language).getExecutionTime()).append(" ");
             else
                 line.append(NON_EXISTING_PROBLEM_PLACEHOLDER).append(" ");
         }
